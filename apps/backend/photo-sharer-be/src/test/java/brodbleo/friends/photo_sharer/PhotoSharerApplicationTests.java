e73f72fdc26ace5ca50c66af009c8e3b9558caab
Léon Brodbeck <brodbeckleon@gmail.com>
2025-08-26 22:43:22 +0200

    inital commit

---
+++ apps/backend/photo-sharer-be/src/test/java/brodbleo/friends/photo_sharer/PhotoSharerApplicationTests.java
@@ -0,0 +1,13 @@
+package brodbleo.friends.photo_sharer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoSharerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
